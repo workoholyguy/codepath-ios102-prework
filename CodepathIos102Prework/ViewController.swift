72bf0601d934bfa032b45caa6356bf331bf24b34
Omar Madjitov <95881196+workoholyguy@users.noreply.github.com>
2025-08-03 15:08:57 -0400

    2nd Commit

--- CodepathIos102Prework/ViewController.swift
+++ CodepathIos102Prework/ViewController.swift
@@ -8,10 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var FirstNameTextField: UITextField!
+    
+    @IBOutlet weak var LastNameTextField: UITextField!
 
+    @IBOutlet weak var SchoolTextField: UITextField!
+    
+    @IBOutlet weak var SchoolYearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var NumberOfPetsLabel: UILabel!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        NumberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBOutlet weak var MorePetsSwitch: UISwitch!
+    
+    
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = SchoolYearSegmentedControl.titleForSegment(
+            at: SchoolYearSegmentedControl.selectedSegmentIndex
+        )
+
+        
+        let introduction = "Hello, my Name is \(FirstNameTextField.text!) \(LastNameTextField.text!) an I attend \(SchoolTextField.text!). I'm currently in my \(year!) year, and I own \(NumberOfPetsLabel.text!) pets. it is \(MorePetsSwitch.isOn) that I want more pets."
+        
+//        print(introduction)
+        
+        let alerController = UIAlertController(
+            title:"Nice to Meet you!",
+            message: introduction, preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title:"Nice to meet you!",
+            style:.default,
+            handler: nil
+        )
+        
+        alerController.addAction(action)
+        
+        present(alerController, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
